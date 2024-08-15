25cf3380355d7f62f4273f1cbdc5ffa85579ccb8
Alexander Close <ardclose@gmail.com>
2024-08-15 17:57:23 +0100

    Adds test_stream_post.py and working user_posting_emulation_streaming.py. Also updates README.md

---
+++ test_stream_post.py
@@ -0,0 +1,35 @@
+import requests
+import json
+from datetime import datetime
+
+example_df = {
+  'index': 7528, 'unique_id': 'fbe53c66-3442-4773-b19e-d3ec6f54dddf',
+  'title': 'No Title Data Available',
+  'description': 'No description available Story format',
+  'poster_name': 'User Info Error', 'follower_count': 'User Info Error',
+  'tag_list': 'N,o, ,T,a,g,s, ,A,v,a,i,l,a,b,l,e',
+  'is_image_or_video': 'multi-video(story page format)',
+  'image_src': 'Image src error.', 'downloaded': 0,
+  'save_location': 'Local save in /data/mens-fashion',
+  'category': 'mens-fashion'
+  }
+# example_df = dict(example_df._mapping)
+# example_df = {key: convert_datetime(val) for key,val in example_df.items()}
+
+invoke_url='https://foyd3wyk4c.execute-api.us-east-1.amazonaws.com/dev/streams/{stream-name}/record'
+ 
+payload = json.dumps({
+    "StreamName": "streaming-126ca3664fbb-pin",
+    "Data": example_df,
+    #{
+    #         #Data should be send as pairs of column_name:value, with different columns separated by commas      
+    #         "index": example_df["index"], "name": example_df["name"],
+    #         "age": example_df["age"], "role": example_df["role"]
+    #         },
+    "PartitionKey": "partition-pin"
+})
+
+headers = {'Content-Type': 'application/json'}
+
+response = requests.request("PUT", invoke_url, headers=headers, data=payload)
+print(response.status_code)
---
+++ user_posting_emulation_streaming.py
@@ -0,0 +1,164 @@
+#########################################################
+# # Code modified from useer_posting_emulation.py
+# Requests changed from POSTs to PUTs
+# API now interacts with Kinesis rather than Kafka
+#########################################################
+
+import requests
+from time import sleep
+import random
+from multiprocessing import Process
+import boto3
+import json
+import sqlalchemy
+from sqlalchemy import text
+import yaml
+from datetime import datetime
+
+# useful to encode datetime entries in JSON
+def convert_datetime(obj):
+    if isinstance(obj,datetime):
+        return obj.isoformat() # converts if datetime
+    return obj                 # else passes
+
+random.seed(100)
+
+# to open external credentials
+with open('db_creds.yaml','r') as f:
+    creds = yaml.safe_load(f)
+
+# streaming URL for single record PUT
+invoke_url='https://foyd3wyk4c.execute-api.us-east-1.amazonaws.com/dev/streams/{stream-name}/record'
+
+class AWSDBConnector:
+
+    def __init__(self):
+        # following refer to credentials in my
+        # db_creds.yaml file ignored by git
+        self.HOST = creds['HOST']
+        self.USER = creds['USER']
+        self.PASSWORD = creds['PASSWORD']
+        self.DATABASE = 'pinterest_data'
+        self.PORT = 3306
+        
+    def create_db_connector(self):
+        engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
+        return engine
+
+
+new_connector = AWSDBConnector()
+
+
+def run_infinite_post_data_loop():
+    while True:
+        sleep(random.randrange(0, 2))
+        random_row = random.randint(0, 11000)
+        engine = new_connector.create_db_connector()
+
+        with engine.connect() as connection:
+
+            # sending pin data to the .pin stream
+
+            pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
+            pin_selected_row = connection.execute(pin_string)
+            
+            for row in pin_selected_row:
+                pin_result = dict(row._mapping)
+                pin_result = {key: convert_datetime(val) for key,val in pin_result.items()}
+                pin_payload = json.dumps({
+                    "StreamName": "streaming-126ca3664fbb-pin",
+                    # Kinesis json structure
+                    "Data": {                 # not "record" as w/ Kafka
+                          "index": pin_result # not "value" as w/ Kakfa
+                          },
+                    "PartitionKey": "partition-pin" # new value
+                })
+            headers = {'Content-Type': 'application/json'}  # not /vnd.kafka.json.v2+json   
+            pin_response = requests.request("PUT",
+                                        invoke_url,
+                                        headers=headers,
+                                        data=pin_payload
+                                        )
+            
+            # sending geo data to the .geo topic
+
+            geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
+            geo_selected_row = connection.execute(geo_string)
+            
+            for row in geo_selected_row:
+                geo_result = dict(row._mapping)
+                geo_result = {key: convert_datetime(val) for key,val in geo_result.items()}
+                geo_payload = json.dumps({
+                    "StreamName": "streaming-126ca3664fbb-geo",
+                    # Kinesis json structure
+                    "Data": {                 # not "record" as w/ Kafka
+                          "index": geo_result # not "value" as w/ Kakfa
+                          },
+                    "PartitionKey": "partition-geo" # new value
+                })
+            headers = {'Content-Type': 'application/json'}  # not /vnd.kafka.json.v2+json  
+            geo_response = requests.request("PUT",
+                                        invoke_url,
+                                        headers=headers,
+                                        data=geo_payload
+                                        )
+            
+            # sending user data to the .user topic
+
+            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
+            user_selected_row = connection.execute(user_string)
+            
+            for row in user_selected_row:
+                user_result = dict(row._mapping)
+                user_result = {key: convert_datetime(val) for key,val in user_result.items()}
+                user_payload = json.dumps({
+                    "StreamName": "streaming-126ca3664fbb-geo",
+                    # Kinesis json structure
+                    "Data": {                 # not "record" as w/ Kafka
+                          "index": geo_result # not "value" as w/ Kakfa
+                          },
+                    "PartitionKey": "partition-geo" # new value
+                })
+            headers = {'Content-Type': 'application/json'}  # not /vnd.kafka.json.v2+json
+            user_response = requests.request("PUT",
+                                        invoke_url,
+                                        headers=headers,
+                                        data=user_payload
+                                        )
+
+            print(
+                [
+                    'CODES: ',
+                    pin_response.status_code,
+                    geo_response.status_code,
+                    user_response.status_code
+                 ]
+                )
+
+
+if __name__ == "__main__":
+    print('Post stream ongoing...')
+    run_infinite_post_data_loop()
+
+if __name__ == "__mai__":
+    invoke_url = "https://foyd3wyk4c.execute-api.us-east-1.amazonaws.com/dev"
+    example_df = {"index": 1, "name": "Maya", "age": 25, "role": "engineer"}
+
+    # invoke_url = "https://YourAPIInvokeURL/YourDeploymentStage/topics/YourTopicName"
+    #To send JSON messages you need to follow this structure
+    payload = json.dumps({
+        "records": [
+            {
+            #Data should be send as pairs of column_name:value, with different columns separated by commas       
+            "value": {"index": example_df["index"], "role": example_df["role"]}
+            }
+        ]
+    })
+
+    headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
+    response = requests.request("POST",
+                                invoke_url+'/topics/126ca3664fbb.pin',
+                                headers=headers,
+                                data=payload)
+    print(response.status_code)
+
